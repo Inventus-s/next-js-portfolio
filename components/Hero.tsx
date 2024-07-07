ea3f213f5e75fcec8d1a177af64bba777c417f61
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-07-07 17:45:11 +0530

    Working on Grid

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
-import ButtonMagic from './ui/ButtonMagic'
+import ButtonMagic from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
