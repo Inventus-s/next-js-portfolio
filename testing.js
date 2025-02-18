10ac4990ed1bec2a9b2670cdc19b5f36bd1a220a
Inventus-s <handle.extras@gmail.com>
2025-02-19 02:10:24 +0530

    but solved

--- testing.js
+++
@@ -1,9 +0,0 @@
-const navItems = ["Mukund", "sahil", "Parveen", "Kutta"];
-
-let no = 0
-// Map -> loop on Array or Onject
-const fn = navItems.map(item => `${item} ${no++}`);
-
-const newItem = [...navItems,"PapaJI"]
-
-console.log(fn);
